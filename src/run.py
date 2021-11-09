9b97460a5cc4af13dbb1efec620fc1e6f46c1bd3
WillShardlow <willjr9@gmail.com>
2021-11-09 16:34:38 +0000

    Deleted redundant code

--- src/run.py
+++ src/run.py
@@ -5,19 +5,6 @@ import tkinter as tk
 
 
 if __name__ == "__main__":
-    # print("""Hello and welcome to YouTube, what would you like to do?
-    # Enter HELP for list of available commands or EXIT to terminate.""")
-    video_player = VideoPlayer()
-    # parser = CommandParser(video_player)
-    # while True:
-    #     command = input("YT> ")
-    #     if command.upper() == "EXIT":
-    #         break
-    #     try:
-    #         parser.execute_command(command.split())
-    #     except CommandException as e:
-    #         print(e)
-    # print("YouTube has now terminated its execution. "
-    #       "Thank you and goodbye!")
 
+    video_player = VideoPlayer()
     gui = GUI(video_player)
